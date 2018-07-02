363b84eb8241d8fd0c71a83a587996e3b4b2f5e3
Hadrien Mens-Pellen <hmens@financeactive.com>
2018-07-02 12:56:25 +0200

    should return even number test function

--- src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/MyTest.java
+++ src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/MyTest.java
@@ -1,10 +1,16 @@
 package fr.hadrienmp.tdd.xp._60.minutes;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 public class MyTest {
     @Test
-    public void myTest2() {
+    public void shouldReturnEvenNumber() {
+        int nombre = checkNumber();
+        Assertions.assertThat(nombre%2).isEqualTo(0);
+    }
 
+    public int checkNumber(){
+        return 0;
     }
 }
