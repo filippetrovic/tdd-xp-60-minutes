1fb4cb4e2ed2de35a006ec71897e570dee9bd4e3
Hadrien Mens-Pellen <hmens@financeactive.com>
2018-07-02 14:47:53 +0200

    The rest of the features - Greater than 9 - Smaller than 100 - Sum of digits is 6

--- src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/MyTest.java
+++ src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/MyTest.java
@@ -15,7 +15,27 @@ public class MyTest {
         int nombre = checkNumber();
         Assertions.assertThat(nombre%10).isEqualTo(2);
     }
+
+    @Test
+    public void souldBeGreaterThan9() {
+        int nombre = checkNumber();
+        Assertions.assertThat(nombre).isGreaterThan(9);
+    }
+
+    @Test
+    public void test2() {
+        int nombre = checkNumber();
+        Assertions.assertThat(nombre).isLessThan(100);
+    }
+
+    @Test
+    public void test3() {
+        int nombre = checkNumber();
+        int dizaine = nombre/10;
+        int unite = nombre%10;
+        Assertions.assertThat(dizaine+unite).isEqualTo(6);
+    }
     public int checkNumber(){
-        return 12;
+        return 42;
     }
 }
